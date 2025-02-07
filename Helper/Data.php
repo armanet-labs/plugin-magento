392b5c3d9fd753a12c7bf0f43019582fb6ccd07b
Ignacio Aguirre <ignacio@independiente.cl>
2025-02-07 14:12:24 -0300

    add plugin config

--- Helper/Data.php
+++ Helper/Data.php
@@ -7,16 +7,32 @@ use Magento\Store\Model\ScopeInterface;
 
 class Data extends AbstractHelper
 {
-    const CONFIG_PATH_API_KEY = 'armanet_integration/general/api_key';
-    const CONFIG_PATH_LOGGING = 'armanet_integration/general/enable_logging';
+    const CONFIG_PATH_API_KEY = 'armanet/settings/api_key';
+    const CONFIG_PATH_ENABLE_TRACKING = 'armanet/settings/enable_tracking';
+    const CONFIG_PATH_ENABLE_FEED = 'armanet/settings/enable_feed';
 
-    public function getApiKey()
+    public function getApiKey($storeId = null)
     {
-        return $this->scopeConfig->getValue(self::CONFIG_PATH_API_KEY, ScopeInterface::SCOPE_STORE);
+        return $this->scopeConfig->getValue(
+           self::CONFIG_PATH_API_KEY,
+           ScopeInterface::SCOPE_STORE,
+           $storeId
+       );
     }
 
-    public function isLoggingEnabled()
+    public function isTrackingEnabled()
     {
-        return $this->scopeConfig->isSetFlag(self::CONFIG_PATH_LOGGING, ScopeInterface::SCOPE_STORE);
+       return $this->scopeConfig->isSetFlag(
+           self::CONFIG_PATH_ENABLE_TRACKING,
+           ScopeInterface::SCOPE_STORE,
+       );
+    }
+
+    public function isFeedEnabled()
+    {
+       return $this->scopeConfig->isSetFlag(
+           self::CONFIG_PATH_ENABLE_FEED,
+           ScopeInterface::SCOPE_STORE,
+       );
     }
 }
