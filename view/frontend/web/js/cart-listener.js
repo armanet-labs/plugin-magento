16ea95bcedb646c789138e9ea7c7463634cf376c
Klaus Silveira <contact@klaussilveira.com>
2025-03-18 14:53:00 -0400

    Do not send events when adding to cart.

--- view/frontend/web/js/cart-listener.js
+++ view/frontend/web/js/cart-listener.js
@@ -5,36 +5,5 @@ define('armanet_cart_listener', [
   'use strict';
 
   return function() {
-    $(document).on('ajax:addToCart', function (event, data) {
-      if (data && data.productIds) {
-        var cart = customerData.get('cart')();
-
-        if (cart && cart.items && cart.items.length) {
-          var addedItem = cart.items.find(item => item.product_id == data.productIds[0]);
-
-          if (addedItem) {
-            sendEventAddedToCart({
-              product_id: addedItem.product_id,
-              product_price_value: addedItem.product_price_value,
-              product_sku: addedItem.product_sku,
-              qty: addedItem.qty,
-            });
-          }
-        }
-      }
-    });
-
-    function sendEventAddedToCart(item) {
-      if (typeof Armanet !== 'undefined' && Armanet && typeof Armanet.sendEvent === 'function') {
-        var transformedPayload = {
-          itemId: item.product_id,
-          upc: item.product_sku,
-          price: item.product_price_value,
-          quantity: item.qty,
-        };
-
-        Armanet.sendEvent('addedToCart', transformedPayload);
-      }
-    }
   };
 });
