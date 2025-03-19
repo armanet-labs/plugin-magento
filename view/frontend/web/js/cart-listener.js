231e4526946414f8cdedaa864f9d36998d40af76
Ignacio Aguirre <ignacio@independiente.cl>
2025-03-19 05:40:39 -0300

    add defensive checks to cart listener

--- view/frontend/web/js/cart-listener.js
+++ view/frontend/web/js/cart-listener.js
@@ -6,13 +6,26 @@ define('armanet_cart_listener', [
 
   return function() {
     $(document).on('ajax:addToCart', function (event, data) {
-      if (data && data.productIds) {
+      if (data && data.productIds && data.productIds.length > 0) {
         var cart = customerData.get('cart')();
 
-        if (cart && cart.items && cart.items.length) {
-          var addedItem = cart.items.find(item => item.product_id == data.productIds[0]);
+        if (cart && cart.items && cart.items.length > 0) {
+          var addedItem = null;
+          for (var i = 0; i < cart.items.length; i++) {
+            var item = cart.items[i];
+            if (item && typeof item.product_id !== 'undefined' && item.product_id == data.productIds[0]) {
+              addedItem = item;
+              break;
+            }
+          }
 
-          if (addedItem) {
+          if (
+            addedItem &&
+            typeof addedItem.product_id !== 'undefined' &&
+            typeof addedItem.product_price_value !== 'undefined' &&
+            typeof addedItem.product_sku !== 'undefined' &&
+            typeof addedItem.qty !== 'undefined'
+          ) {
             sendEventAddedToCart({
               product_id: addedItem.product_id,
               product_price_value: addedItem.product_price_value,
