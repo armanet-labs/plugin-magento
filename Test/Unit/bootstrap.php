9196a966484dc6f7699d96d0883312aaca8d3a52
Ignacio Aguirre <ignacio@independiente.cl>
2025-04-13 03:31:40 -0400

    remove comments

--- Test/Unit/bootstrap.php
+++ Test/Unit/bootstrap.php
@@ -1,8 +1,6 @@
 <?php
-// require_once realpath(__DIR__ . '/../../../../vendor/autoload.php');
 require_once __DIR__ . '/../../../../../autoload.php';
 
 if (!defined('TESTS_TEMP_DIR')) {
-    // define('TESTS_TEMP_DIR', dirname(__DIR__) . '/tmp');
     define('TESTS_TEMP_DIR', dirname(__DIR__));
 }
